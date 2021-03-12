4dea6a12c1ca4b59312c378a1e9e46a5a344a769
Alex <alabandy@gmail.com>
2021-03-12 13:41:14 -0500

    Initial commit

---
+++ src/test/java/Tests/Stimware.java
@@ -0,0 +1,32 @@
+package Tests;
+
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import Pagefactory.PageFactoryStimwave;
+import com.codeborne.selenide.Configuration;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Stimware {
+    @BeforeClass
+    void configurations() {
+        //Configuration.headless=true;
+        Configuration.browser = WebDriverRunner.FIREFOX;
+        //Configuration.browserBinary=("C:/Program Files/Google/Chrome Beta/Application/chrome.exe");
+    }
+    @BeforeMethod
+    void setup() {
+        String baseUrl = "https://stimwavefreedom.com/";
+        open(baseUrl);
+    }
+    @Test
+    void Test1() throws Exception {
+        PageFactoryStimwave contactForm = new PageFactoryStimwave();
+        contactForm.contact();
+        sleep(3000);
+        screenshot("Steam");
+    }
+}
