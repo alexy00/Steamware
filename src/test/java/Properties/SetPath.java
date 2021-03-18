c38a8a00312ba41fa3cc9a5a67b2fe424d9f5038
Alex <alabandy@gmail.com>
2021-03-18 12:09:12 -0400

    Initial commit

--- src/test/java/Properties/SetPath.java
+++ src/test/java/Properties/SetPath.java
@@ -2,7 +2,7 @@ package Properties;
 
 public class SetPath {
     public static String Stimware(){
-        return "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins.jenkins/workspace/Stimwave/build/reports/tests/Fotos";
+        return "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins.jenkins/workspace/Steamwave/build/reports/tests/Fotos";
     }
 
 }
