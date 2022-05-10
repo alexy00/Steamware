9b6ddca93793a25e6e10fd009788ad84702fff5c
Alex <alabandy@gmail.com>
2022-05-10 13:59:55 -0400

    Initial commit

--- src/test/java/Pagefactory/PageFactoryStimwave.java
+++ src/test/java/Pagefactory/PageFactoryStimwave.java
@@ -15,31 +15,22 @@ public class PageFactoryStimwave {
         contactBtn.click();
         SelenideElement fname = $(byId("swcontactform-firstname"));
         fname.sendKeys("First");
-
         SelenideElement lname = $(byId("swcontactform-lastname"));
         lname.sendKeys("Last");
-
         SelenideElement city = $(byId("swcontactform-city"));
         city.sendKeys("City");
-
         SelenideElement state = $(byId("swcontactform-state"));
         state.sendKeys("State");
-
         SelenideElement zip = $(byId("swcontactform-zip"));
         zip.sendKeys("11111");
-
         SelenideElement email = $(byId("swcontactform-email"));
         email.sendKeys("email@gmail.com");
-
         SelenideElement phone = $(byId("swcontactform-phone"));
         phone.sendKeys("1231231235");
-
         SelenideElement selector = $(byId("swcontactform-pain_area"));
         selector.selectOptionByValue("2");
-
         SelenideElement referral = $(byId("swcontactform-referral"));
         referral.selectOptionByValue("4");
-
         switchTo().frame($x("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.google.com/recaptcha')]"));
         //$(".recaptcha-checkbox-border").click();
        $("span[role='checkbox']")
